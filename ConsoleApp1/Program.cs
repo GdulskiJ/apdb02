d3d15c49ca20c64f8df36881887162f444853f29
Jakub Gdulski <s27753@pjwstk.edu.pl>
2024-03-08 14:59:04 +0100

    zadanie

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello");
+Console.WriteLine(GetAvg([1,2,3,4,5]));
+
+    static double GetAvg(int[] arr)
+    {
+        int sum = 0;
+        foreach (var num in arr)
+        {
+            sum += num;
+        }
+
+        return (double)sum / arr.Length;git status
+    }
